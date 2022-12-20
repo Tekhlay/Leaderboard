41ef3b0d4492f52a7139b8d7276f20759442ab10
Teklay <teklay@gmail.com>
2022-12-20 17:29:43 +0300

    Add css loader bundler

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
