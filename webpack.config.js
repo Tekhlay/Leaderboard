675b0710f09957f6d0d1b9687f9b31740cb485b6
Teklay <teklay@gmail.com>
2022-12-20 17:08:16 +0300

    Setup webpack bundler

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
