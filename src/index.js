04e441b9af4f9aa36d972c2acea2f6bde64bee3a
Teklay <teklay@gmail.com>
2022-12-22 03:37:19 +0300

    Style layout of the leaderboard wireframe

--- src/index.js
+++ src/index.js
@@ -1,19 +1,59 @@
+import { indexOf } from 'lodash';
 import './style.css';
 
 let data = {};
 const id = 'dLQGiu9bKl2Xwh0Docr4';
+// const froms = document.querySelector('.form-score');
 const allscore = document.querySelector('.list-score');
 const refresh = document.querySelector('.btn-refresh');
 const addscore = document.querySelector('.addbtn');
-const user = document.querySelector('#user');
-const score = document.querySelector('#score');
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`;
+
+const errors = document.querySelector('.errorscore');
+const erroru = document.querySelector('.erroruser');
+const sucess = document.querySelector('.success');
+const erroMsgs = () => {
+  const escore = document.createElement('strong');
+  escore.className = 'escore';
+  errors.style.color = 'red';
+  escore.innerHTML = ' Score is reqired!';
+  errors.appendChild(escore);
+  setTimeout(() => {
+    document.querySelector('.escore').remove();
+  }, 2000);
+};
+const erroMsgu = () => {
+  const span = document.createElement('strong');
+  span.className = 'errorus';
+  erroru.style.color = 'red';
+  span.innerHTML = 'Name is reqired!';
+  erroru.appendChild(span);
+  setTimeout(() => {
+    document.querySelector('.errorus').remove();
+  }, 2000);
+};
+const Sucess = () => {
+  const s = document.createElement('p');
+  s.className = 'sucessmsg';
+  sucess.style.display = 'block';
+  s.textContent = 'Data Added sucessfully!';
+  sucess.style.color = 'white';
+  sucess.appendChild(s);
+  setTimeout(() => {
+    document.querySelector('.sucessmsg').remove();
+  }, 2000);
+};
+
 const fetchData = async () => {
   const gamescore = await fetch(url);
   const gamedata = await gamescore.json();
   allscore.innerHTML = '';
   gamedata.result.forEach((element) => {
-    allscore.innerHTML += `<tr><td>${element.user}</td><td>${element.score}</td></tr>`;
+    allscore.innerHTML += `<tr id="${gamedata.result.indexOf(element)}"><td>${element.user}</td><td>${element.score}</td></tr>`;
+    if (gamedata.result.indexOf(element) % 2 === 0) {
+      const c = document.getElementById(`${gamedata.result.indexOf(element)}`);
+      c.style.background = 'grey';
+    }
   });
 };
 
@@ -33,18 +73,22 @@ refresh.addEventListener('click', (element) => {
 });
 
 addscore.addEventListener('click', (e) => {
+  const usern = document.querySelector('#user');
+  const scoreg = document.querySelector('#score');
   e.preventDefault();
-  if (user.value === '' && score.value === '') {
-    // eslint-disable-next-line no-alert
-    alert('Required fields!');
+  if (!usern.value) {
+    erroMsgu();
+  } else if (!scoreg.value) {
+    erroMsgs();
   } else {
     const data1 = {
-      user: user.value,
-      score: score.value,
+      user: usern.value,
+      score: scoreg.value,
     };
     data = data1;
     sendData();
-    user.value = '';
-    score.value = '';
+    usern.value = '';
+    scoreg.value = '';
+    Sucess();
   }
 });
